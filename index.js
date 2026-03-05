f84365f85b9a87b2bb840f1ac5400c883a60e835
Adriano <adrianosenai604@gmail.com>
2026-03-05 20:33:36 -0300

    feat(auth): adicionar login com google
    adiciona autenticação via oauth2 para login com google.
    inclui função simulada para retorno de sucesso.
    
    closes #1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('sistema iniciado');
+
+function loginGoogle() {
+    return "login realizado com sucesso via google oauth2"
+}
+
+module.exports = loginGoogle;
+
+git commit -m "feat(auth): adicionar login com google" \
+-m "adiciona autenticação via oauth2 para login com google".\
+-m "closes #1"
